869ae0e808663024656adc235ce7e2676ac7f9bc
lozsve <793093806@qq.com>
2022-09-17 19:10:59 +0800

    ele设置中文

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="zhCn">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+</script>
 
 <style lang="scss">
 @import '@/styles/flex.css'; /* flex 布局 */
